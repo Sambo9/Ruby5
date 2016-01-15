dfd21baf59b75d905d980ccd4dac1cfa55d2b72e
Sambo <losangeles9@gmail.com>
2016-01-15 17:52:13 +0100

    ex_4

---
+++ ex_4/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username, length: {in: 2..15}
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, length: {in: 2..25}, presence: true
+  validates_confirmation_of :password_confirmation, :message => " password should match confirmation"
+end
