6814c75b5a277462cccb62740e58fe1f0f405ab6
Sambo <losangeles9@gmail.com>
2016-01-15 19:11:52 +0100

    ex_5

---
+++ ex_6/app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ActiveRecord::Base
+  attr_accessor :remember_token
+  has_secure_password
+
+  validates :username, length: {in: 2..15}
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, length: {in: 2..25}, presence: true
+  validates_confirmation_of :password_confirmation, :message => " password should match confirmation"
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+end
